de88b1a73816d4ab82c25dc71a270ea2ac676309
ampersand-github <ampersand.dev@gmail.com>
2023-06-02 08:24:11 +0900

    feat: フォーマットかけた

--- src/env.mjs
+++ src/env.mjs
@@ -2,7 +2,7 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-  server: { DATABASE_URL: z.string().min(1)},
+  server: { DATABASE_URL: z.string().min(1) },
   client: {},
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
