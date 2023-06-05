6ffe694e5b2be6ef629dcb0b7051e86ce54bb254
ampersand-github <ampersand.dev@gmail.com>
2023-06-05 16:51:51 +0900

    feat: テストまで

--- src/app/(operation-check)/operation-check/_components/jest-prisma-check/jest-prisma-check.test.ts
+++ src/app/(operation-check)/operation-check/_components/jest-prisma-check/jest-prisma-check.test.ts
@@ -2,8 +2,18 @@ import { db } from "@/lib/db/db";
 
 describe("jest-prisma-check", () => {
   test("userテーブルが存在するとき、trueを返すべき", async () => {
+    await db.user.create({
+      data: {
+        id: "2",
+      },
+    });
+    const one = await db.user.findUnique({
+      where: {
+        id: "2",
+      }
+    })
+    console.log(one)
     const count = await db.user.count();
-    // console.log("count", count)
     expect(count >= 0).toBe(true);
   });
 });
