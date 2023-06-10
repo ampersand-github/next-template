a91462eed6bed9017f11279752ee0072cd28ef2d
ampersand-github <ampersand.dev@gmail.com>
2023-06-10 13:19:45 +0900

    feat: t3envをstorybookから削除

--- src/app/(operation-check)/operation-check/image/image-download/_components/download-image/index.stories.tsx
+++ src/app/(operation-check)/operation-check/image/image-download/_components/download-image/index.stories.tsx
@@ -1,11 +1,11 @@
-import { env } from "@/env.mjs";
 import type { Meta, StoryObj } from "@storybook/react";
 import { rest } from "msw";
 import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 import { Suspense } from "react";
 import { DownloadImage } from "./index";
 
-const origin = env.NEXT_PUBLIC_ORIGIN;
+// const origin = env.NEXT_PUBLIC_ORIGIN;
+const origin = "http://localhost:3000";
 const nextApiUrl = `${origin}/api/storage/simple/download`;
 const dummyPhoto =
   "https://plus.unsplash.com/premium_photo-1686050416689-1b1f64fd5000";
