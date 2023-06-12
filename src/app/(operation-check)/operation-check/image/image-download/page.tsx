c86cccf9f4eaaeb97ded425ad9f5c29679e3415b
ampersand-github <ampersand.dev@gmail.com>
2023-06-12 22:00:34 +0900

    feat: ビルドが失敗する問題を解決

--- src/app/(operation-check)/operation-check/image/image-download/page.tsx
+++ src/app/(operation-check)/operation-check/image/image-download/page.tsx
@@ -1,12 +1,11 @@
 import { Suspense } from "react";
-import { DownloadImage } from "./_components";
 
-export default function Page() {
+export default async function Page() {
   return (
     <main className="flex flex-col space-y-8 p-24">
       <h1 className="text-4xl font-bold">download-image</h1>
       <Suspense fallback={<div>loading...</div>}>
-        <DownloadImage />
+        {/* <DownloadImage /> */}
       </Suspense>
     </main>
   );
