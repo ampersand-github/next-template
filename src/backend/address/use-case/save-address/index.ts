de9ac62d17bd8226ec0a030ede552b473a2f8f4d
ampersand-github <ampersand.dev@gmail.com>
2023-06-28 19:42:31 +0900

    feat: バックエンド完成

--- src/backend/address/use-case/save-address/index.ts
+++ src/backend/address/use-case/save-address/index.ts
@@ -1,43 +1,34 @@
 import { Address, IAddress } from "@/backend/address/domain";
 import { AddressRepository } from "@/backend/address/repository";
 import { UserId } from "@/backend/user";
-import { Result, Success } from "@/lib/result";
 import { PrismaClient } from "@prisma/client";
+import { ISaveAddressRequest } from "./request-interface";
+import { ISaveAddressResponse } from "./response-interface";
 
-type RequestType = {
-  address: {
-    postalCode: string;
-    prefecture: string;
-    city: string;
-    town: string;
-    block: string;
-  };
-  userId: string;
-};
+type Props = ISaveAddressRequest & { userId: string };
 
-type ResponseType = {
-  postalCode: string;
-  prefecture: string;
-  city: string;
-  town: string;
-  block: string;
-};
-
-export class AddressSaveUseCase {
+/**
+ * @public
+ */
+export class SaveAddress {
   private repository: AddressRepository = new AddressRepository();
   constructor(private readonly prisma: PrismaClient) {}
   async execute({
-    address,
+    postalCode,
+    prefecture,
+    city,
+    town,
+    block,
     userId,
-  }: RequestType): Promise<Result<ResponseType, Error>> {
+  }: Props): Promise<ISaveAddressResponse> {
     // データを作成
     const _userId = UserId.reBuild(userId);
     const props: IAddress = {
-      postalCode: address.postalCode,
-      prefecture: address.prefecture,
-      city: address.city,
-      town: address.town,
-      block: address.block,
+      postalCode: postalCode,
+      prefecture: prefecture,
+      city: city,
+      town: town,
+      block: block,
     };
     const _address: Address = Address.reBuild(props, _userId);
 
@@ -45,13 +36,12 @@ export class AddressSaveUseCase {
     const result: Address = await this.repository.save(this.prisma, _address);
 
     // 返却
-    const res: ResponseType = {
+    return {
       postalCode: result.postalCode,
       prefecture: result.prefecture,
       city: result.city,
       town: result.town,
-      block: result.block,
+      block: result.block ?? "",
     };
-    return new Success(res);
   }
 }
