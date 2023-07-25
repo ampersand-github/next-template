187db7a754f3ac8e4e13778e4b180cd987e0d77c
ampersand-github <ampersand.dev@gmail.com>
2023-07-25 23:58:43 +0900

    feat: shadcnのあっぷでーと

--- src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-/**
- * @public
- */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
---
+++ src/lib/utils/cn.ts
@@ -0,0 +1,9 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+/**
+ * @public
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
