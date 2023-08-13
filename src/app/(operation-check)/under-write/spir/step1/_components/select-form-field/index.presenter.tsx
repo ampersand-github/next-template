f66f66e11e49fee97cf5c48b80e59712a1acb921
ampersand-github <ampersand.dev@gmail.com>
2023-08-13 11:12:33 +0900

    feat: step1

---
+++ src/app/(operation-check)/under-write/spir/step1/_components/select-form-field/index.presenter.tsx
@@ -0,0 +1,73 @@
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/__shared__/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/__shared__/components/ui/select";
+import { UseFormReturn } from "react-hook-form";
+import { z } from "zod";
+import { Option, stepOneSchema } from "../index";
+
+type Props = {
+  form: UseFormReturn<z.infer<typeof stepOneSchema>>;
+  name: keyof z.infer<typeof stepOneSchema>;
+  label: string;
+  placeholder: string;
+  description: string;
+  options: Record<string, Option>;
+  onValueChange?: (value: string) => void;
+};
+
+export const SelectFormFieldPresenter = ({
+  form,
+  name,
+  label,
+  placeholder,
+  description,
+  options,
+  onValueChange,
+}: Props) => {
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <Select
+            onValueChange={onValueChange || field.onChange}
+            value={field.value}
+          >
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue
+                  placeholder={
+                    <p className={"text-muted-foreground"}>{placeholder}</p>
+                  }
+                />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              {Object.values(options).map((option: Option) => (
+                <SelectItem key={option.value} value={option.value}>
+                  {option.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <FormDescription>{description}</FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
