ab3bcec2c49fa057ee2c0737fa7e5d7c36284b5b
ampersand-github <ampersand.dev@gmail.com>
2023-10-16 08:49:25 +0900

    feat: ビルドが通らないのを解消

--- src/app/(ct)/y/page.tsx
+++ src/app/(ct)/y/page.tsx
@@ -2,17 +2,10 @@ import { url } from "@/__shared__/utils/url";
 import { Container } from "@/app/(ct)/y/_components";
 import { env } from "@/env.mjs";
 
-const fetchPrefectures = async (apiKey: string, url: string) => {
-  const result = await fetch(url, { headers: { "X-API-KEY": apiKey } });
-  const json = await result.json();
-  return json.result;
-};
-
 export default async function Home() {
   const apiKey = env.RESAS_API_KEY;
   const prefecturesUrl = url.RESAS_BASE_URL + "/prefectures";
-
-  const prefectures = await fetchPrefectures(apiKey, prefecturesUrl);
-
-  return <Container apiKey={apiKey} prefectures={prefectures} />;
+  const result = await fetch(prefecturesUrl, { headers: { "X-API-KEY": apiKey } });
+  const json = await result.json();
+  return <Container apiKey={apiKey} prefectures={json} />;
 }
